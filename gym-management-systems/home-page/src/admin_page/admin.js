4025c0190f49bdfe35c39cb7292d3a418b9b7625
OnichanJay <brendancondes@gmail.com>
2024-07-08 22:43:14 +0800

    [update][condes]
    update dashboard

--- gym-management-systems/home-page/src/admin_page/admin.js
+++ gym-management-systems/home-page/src/admin_page/admin.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import Background from '../admin_page/AdminImages/BackgroundWhite.png';
+import AdminBackground from '../admin_page/AdminImages/AdminBoard.png';
+
 
 function Adminpage() {
     return (
-        <div></div>
 
+        <div>
 
+        {/*  AdminWhiteBackground   */}
+        <img
+        loading="lazy"
+        src={Background}
+        className="background"
+        alt="background"
+        />
+
+        {/*  AdminBackground    */}
+        <img
+        loading="lazy"
+        src={AdminBackground}
+        className="adminboard"
+        alt="adminbackground"
+        />
+
+        </div>
     );
 
 
